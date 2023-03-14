5119667aa2183e801e8618c9b4f66f30227dfdd1
hieugunny <hieult420@uef.edu.vn>
2023-03-14 13:44:06 +0700

    Add project files.

---
+++ Lab03_Movie/Data/Lab03_MovieContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Lab03_Movie.Models;
+using Microsoft.Extensions.Logging;
+
+namespace Lab03_Movie.Data
+{
+    public class Lab03_MovieContext : DbContext
+    {
+        public Lab03_MovieContext (DbContextOptions<Lab03_MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lab03_Movie.Models.Movie> Movie { get; set; } = default!;
+         
+    }
+}
---
+++ Lab03_Movie/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Lab03_Movie.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Lab03_MovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Lab03_MovieContext") ?? throw new InvalidOperationException("Connection string 'Lab03_MovieContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Movies}/{action=Index}/{id?}");
+
+app.Run();
